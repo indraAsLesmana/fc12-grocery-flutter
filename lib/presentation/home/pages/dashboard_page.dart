ec4aac0f9914d8fd389adcb936ae9d117dd5e3fa
Indra Agus Lesmana <indra.lesmana07@homecredit.co.id>
2024-01-19 04:55:23 +0700

    add bottombar menu

--- lib/presentation/home/pages/dashboard_page.dart
+++ lib/presentation/home/pages/dashboard_page.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
+import '../../../core/assets/assets.gen.dart';
+import '../../../core/constants/colors.dart';
+
 class DashboardPage extends StatefulWidget {
   final int currentTab;
-
   const DashboardPage({
     Key? key,
     required this.currentTab,
@@ -14,8 +16,88 @@ class DashboardPage extends StatefulWidget {
 }
 
 class _DashboardPageState extends State<DashboardPage> {
+  late int _selectedIndex;
+  final List<Widget> _pages = [
+    // const HomePage(),
+    // const SearchPage(),
+    // const CartPage(),
+    // const Center(
+    //   child: Text('This Page 1'),
+    // ),
+    const Center(
+      child: Text('This Page 2'),
+    ),
+    const Center(
+      child: Text('This Page 3'),
+    ),
+    const Center(
+      child: Text('This Page 4 '),
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    _selectedIndex = index;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    _selectedIndex = widget.currentTab;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: IndexedStack(index: _selectedIndex, children: _pages),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        selectedItemColor: AppColors.primary,
+        onTap: _onItemTapped,
+        items: [
+          BottomNavigationBarItem(
+            icon: Assets.icons.home.svg(
+              colorFilter: const ColorFilter.mode(
+                AppColors.grey,
+                BlendMode.srcIn,
+              ),
+            ),
+            activeIcon: Assets.icons.home.svg(),
+            label: 'HOME',
+          ),
+          BottomNavigationBarItem(
+            icon: Assets.icons.search.svg(
+              colorFilter: const ColorFilter.mode(
+                AppColors.grey,
+                BlendMode.srcIn,
+              ),
+            ),
+            activeIcon: Assets.icons.search.svg(),
+            label: 'EXPLORE',
+          ),
+          BottomNavigationBarItem(
+            icon: Assets.icons.order.svg(
+              colorFilter: const ColorFilter.mode(
+                AppColors.grey,
+                BlendMode.srcIn,
+              ),
+            ),
+            activeIcon: Assets.icons.order.svg(),
+            label: 'ORDER',
+          ),
+          BottomNavigationBarItem(
+            icon: Assets.icons.person.svg(
+              colorFilter: const ColorFilter.mode(
+                AppColors.grey,
+                BlendMode.srcIn,
+              ),
+            ),
+            activeIcon: Assets.icons.person.svg(),
+            label: 'ACCOUNT',
+          ),
+        ],
+      ),
+    );
   }
 }
