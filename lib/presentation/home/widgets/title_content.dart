0ef7bf4f8c03f05b87c79a582a170ff89835d398
Indra Agus Lesmana <indra.lesmana07@homecredit.co.id>
2024-01-26 22:54:23 +0700

    remove see all on categories section

--- lib/presentation/home/widgets/title_content.dart
+++ lib/presentation/home/widgets/title_content.dart
@@ -24,17 +24,19 @@ class TitleContent extends StatelessWidget {
             fontSize: 16.0,
           ),
         ),
-        InkWell(
-          onTap: onSeeAllTap,
-          child: const Text(
-            'See All',
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w500,
-              color: AppColors.primary,
-            ),
-          ),
-        ),
+        title == 'Categories'
+            ? const SizedBox.shrink()
+            : InkWell(
+                onTap: onSeeAllTap,
+                child: const Text(
+                  'See All',
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w500,
+                    color: AppColors.primary,
+                  ),
+                ),
+              ),
       ],
     );
   }
